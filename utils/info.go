3a363818fcd90cae9855ac152e488737eb11f088
Phil <phil@ucode.space>
2021-12-11 15:19:32 +0100

    Added Info for MR

--- utils/info.go
+++ utils/info.go
@@ -3,8 +3,11 @@ package utils
 import "fmt"
 
 func Info() {
-	fmt.Println("------ GLY.ONE CLI ------")
-	fmt.Println("--- Usage:")
-	fmt.Println("gly <url>  // Shorten a URL")
-	fmt.Println("---------------------------------")
+	fmt.Println("----- gosh CLI -----")
+	fmt.Println("gly <url>        // Shorten a URL")
+	fmt.Println("gly look <id>    // Lookup a ID / Return the URL")
+	fmt.Println("gly get <url>    // Get the original URL from a shortened one")
+	fmt.Println("gly config       // Configure CLI (Removes existing config)")
+	fmt.Println("gly help         // Show this help")
+	fmt.Println("--------------------")
 }
